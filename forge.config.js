d335332e632b0ee796cf0dec16fb4fd7510e1985
Nico Tejera <tejera.nicolas@gmail.com>
2023-01-06 10:05:22 -0500

    dmg without app

--- forge.config.js
+++ forge.config.js
@@ -24,10 +24,10 @@ module.exports = {
       name: "@electron-forge/maker-squirrel",
       config: {},
     },
-    {
-      name: "@electron-forge/maker-zip",
-      platforms: ["darwin"],
-    },
+    // {
+    //   name: "@electron-forge/maker-zip",
+    //   platforms: ["darwin"],
+    // },
     {
       name: "@electron-forge/maker-deb",
       config: {},
